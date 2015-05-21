08fb91b4eaab40d164565c5d6f215aa34dcdcc63
icbat <cobb.evan@gmail.com>
2015-05-21 15:02:39 -0500

    filter list is readable

--- installer/installer.go
+++ installer/installer.go
@@ -1,51 +1,67 @@
 package main
 
 import (
-	"bufio"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
+type filter struct {
+	DisplayName string `json:"displayName"`
+	FilterURL   string `json:"filterURL"`
+	FilterName  string `json:"filterName"`
+}
+
 func main() {
+	// path := getInstallPath()
+
+	filterList := getFilters()
+
+	printMenu(filterList)
+	// filter := getUserSelection()
+
+	// filterName := "ThioleLootFilter.filter"
+	// filterURL := "https://raw.githubusercontent.com/icbat/LootFilter/master/" + filterName
+	// fmt.Println("Grabbing " + filterName + " from: " + filterURL)
+	// downloadTo(filterURL, path+"/"+filterName)
+	//
+	// fmt.Println(`Done!
+	//
+	// To finish the install:
+	//
+	// 1) Restart Path of Exile Beta if you have it open
+	// 2) Start game
+	// 3) Login to Beta
+	// 4) Esc > Options
+	// 5) GO to UI tab, at the bottom select the filter, once it says "Filter loaded successfully" no restart required, you are good to go
+	// 6) Any updates to the filter can be reloaded without restarting the game by clicking the "reload" button in options
+	//
+	//
+	// Press any key to exit!`)
+	// reader := bufio.NewReader(os.Stdin)
+	// reader.ReadString('\n')
+}
+
+func getInstallPath() string {
 	fmt.Println("Checking environment")
 	basePath := os.Getenv("UserProfile")
 	if basePath == "" {
-		fmt.Println(" -- Couldn't resolve environment variables!")
-		return
+		panic(" -- Couldn't resolve environment variables!")
 	}
 	fmt.Println("Found base path of: " + basePath)
 
 	fmt.Println("Verifying directories")
 	path := basePath + "/Documents/My Games/Path of Exile"
 	if !pathExists(path) {
-		fmt.Println(" -- Couldn't find a folder at " + path)
-		return
+		panic(" -- Couldn't find a folder at " + path)
 	}
 	fmt.Println("Found PoE folder at: " + path)
 
 	fmt.Println("Reticulating splines")
-	filterName := "ThioleLootFilter.filter"
-	filterURL := "https://raw.githubusercontent.com/icbat/LootFilter/master/" + filterName
-	fmt.Println("Grabbing " + filterName + " from: " + filterURL)
-	downloadTo(filterURL, path+"/"+filterName)
-
-	fmt.Println(`Done!
-
-	To finish the install:
-
-	1) Restart Path of Exile Beta if you have it open
-	2) Start game
-	3) Login to Beta
-	4) Esc > Options
-	5) GO to UI tab, at the bottom select the filter, once it says "Filter loaded successfully" no restart required, you are good to go
-	6) Any updates to the filter can be reloaded without restarting the game by clicking the "reload" button in options
-
-
-	Press any key to exit!`)
-	reader := bufio.NewReader(os.Stdin)
-	reader.ReadString('\n')
+	return path
 }
 
 func pathExists(path string) bool {
@@ -54,6 +70,34 @@ func pathExists(path string) bool {
 		return true
 	}
 	return false
+}
+
+func getFilters() []filter {
+
+	rawJSON, err := ioutil.ReadFile("filters.json")
+	if err != nil {
+		panic(err)
+	}
+
+	var filterList []filter
+	err = json.Unmarshal(rawJSON, &filterList)
+	if err != nil {
+		panic(err)
+	}
+	return filterList
+}
+
+func printMenu(filters []filter) {
+	fmt.Println("\nAvailable filters:")
+	for index, filter := range filters {
+		fmt.Printf("\t%d : %s\n", index+1, filter.DisplayName)
+	}
+}
+
+func cleanText(text string) string {
+	text = strings.Replace(text, "\n", "", -1)
+	text = strings.Replace(text, "\r", "", -1)
+	return text
 }
 
 func downloadTo(url string, targetFile string) {
